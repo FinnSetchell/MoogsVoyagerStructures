9c92d5c3943ee8759e0f09bdadf95f74ad5f5756
FinnSetchell <88200552+FinnSetchell@users.noreply.github.com>
2022-08-07 15:39:25 +0100

    changes (104 files) changed the MID to mvs (moogs vanilla structures) updated cart.nbt

---
+++ src/main/java/com/finndog/mvs/STStructures.java
@@ -0,0 +1,18 @@
+package com.finndog.mvs;
+
+public class STStructures {
+
+    /**
+    /**
+     * Registers the structure itself and sets what its path is. In this case, the
+     * structure will have the Identifier of structure_tutorial:sky_structures.
+     *
+     * It is always a good idea to register your Structures so that other mods and datapacks can
+     * use them too directly from the registries. It great for mod/datapacks compatibility.
+     */
+
+    public static void registerStructureFeatures() {
+        // The generation step for when to generate the structure. there are 10 stages you can pick from!
+        // This surface structure stage places the structure before plants and ores are generated.
+    }
+}
---
+++ src/main/java/com/finndog/mvs/StructureMain.java
@@ -0,0 +1,21 @@
+package com.finndog.mvs;
+
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class StructureMain implements ModInitializer {
+
+    public static final Logger LOGGER = LogManager.getLogger();
+    public static final String MODID = "mvs";
+
+    @Override
+    public void onInitialize() {
+
+        /*
+         * We setup and register our structures here.
+         * You should always register your stuff to prevent mod compatibility issue down the line.
+         */
+        STStructures.registerStructureFeatures();
+    }
+}
