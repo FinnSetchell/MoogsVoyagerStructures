28013d394aa03a0a41bf009aefeafb29d97f2d24
FinnSetchell <88200552+FinnSetchell@users.noreply.github.com>
2022-08-10 10:54:07 +0100

    tried removing floatingislands but still getting error

--- src/main/java/com/finndog/mvs/MVSStructures.java
+++ src/main/java/com/finndog/mvs/MVSStructures.java
@@ -1,10 +1,6 @@
 package com.finndog.mvs;
 
 import com.finndog.mvs.mixin.StructureFeatureAccessor;
-import com.finndog.mvs.structures.Floatingislands;
-import com.finndog.mvs.structures.Floatingislands;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 import net.minecraft.world.gen.GenerationStep;
 import net.minecraft.world.gen.feature.StructureFeature;
 
@@ -18,11 +14,12 @@ public class MVSStructures {
      * It is always a good idea to register your Structures so that other mods and datapacks can
      * use them too directly from the registries. It great for mod/datapacks compatibility.
      */
-    public static StructureFeature<?> FLOATING_ISLANDS = new Floatingislands();
+    //public static StructureFeature<?> FLOATING_ISLANDS = new Floatingislands();
 
     public static void registerStructureFeatures() {
         // The generation step for when to generate the structure. there are 10 stages you can pick from!
         // This surface structure stage places the structure before plants and ores are generated.
-        StructureFeatureAccessor.callRegister(MVSMain.MODID + ":floatingislands", FLOATING_ISLANDS, GenerationStep.Feature.SURFACE_STRUCTURES);
+
+        //StructureFeatureAccessor.callRegister(MVSMain.MODID + ":floating_islands", FLOATING_ISLANDS, GenerationStep.Feature.SURFACE_STRUCTURES);
     }
 }
