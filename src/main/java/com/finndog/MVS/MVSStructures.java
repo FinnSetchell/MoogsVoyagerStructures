53c328ce3a8eef6aec5de7006c8b95ab60eaa736
FinnSetchell <88200552+FinnSetchell@users.noreply.github.com>
2022-10-06 22:25:29 +0100

    changes

--- src/main/java/com/finndog/MVS/MVSStructures.java
+++ src/main/java/com/finndog/MVS/MVSStructures.java
@@ -11,8 +11,6 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 
 public class MVSStructures {
-    //public static final DeferredRegister<StructureFeature<?>> DEFERRED_REGISTRY_STRUCTURE = DeferredRegister.create(ForgeRegistries.STRUCTURE_FEATURES, MVSMain.MODID);
-    //public static final RegistryObject<StructureFeature<?>> SKY_STRUCTURES = DEFERRED_REGISTRY_STRUCTURE.register("sky_structures", SkyStructures::new);
     /**
      * We are using the Deferred Registry system to register our structure as this is the preferred way on Forge.
      * This will handle registering the base structure for us at the correct time so we don't have to handle it ourselves.
@@ -24,12 +22,8 @@ public class MVSStructures {
      * Registers the base structure itself and sets what its path is. In this case,
      * this base structure will have the resourcelocation of structure_tutorial:sky_structures.
      */
-
-    public static final RegistryObject<StructureType<MVSGenericNetherJigsawStructure>> SKY_STRUCTURES = DEFERRED_REGISTRY_STRUCTURE.register("sky_structures", () -> () -> MVSGenericNetherJigsawStructure.CODEC);
-    public static final RegistryObject<StructureType<MVSYLevelDownOne>> MVS_YLEVEL_DOWN_ONE = DEFERRED_REGISTRY_STRUCTURE.register("mvs_ylevel_down_one", () -> () -> MVSYLevelDownOne.CODEC);
     public static final RegistryObject<StructureType<MVSGenericNetherJigsawStructure>> MVS_GENERIC_JIGSAW_STRUCTURE = DEFERRED_REGISTRY_STRUCTURE.register("mvs_generic_jigsaw_structure", () -> () -> MVSGenericNetherJigsawStructure.CODEC);
     public static final RegistryObject<StructureType<MVSGenericSmallJigsawStructure>> MVS_GENERIC_SMALL_JIGSAW_STRUCTURE = DEFERRED_REGISTRY_STRUCTURE.register("mvs_generic_small_jigsaw_structure", () -> () -> MVSGenericSmallJigsawStructure.CODEC);
     public static final RegistryObject<StructureType<MVSGenericNetherJigsawStructure>> MVS_GENERIC_NETHER_JIGSAW_STRUCTURE = DEFERRED_REGISTRY_STRUCTURE.register("mvs_generic_nether_jigsaw_structure", () -> () -> MVSGenericNetherJigsawStructure.CODEC);
-    public static final RegistryObject<StructureType<MVSGenericOceanJigsawStructure>> MVS_GENERIC_OCEAN_JIGSAW_STRUCTURE = DEFERRED_REGISTRY_STRUCTURE.register("mvs_generic_ocean_jigsaw_structure", () -> () -> MVSGenericOceanJigsawStructure.CODEC);
 
 }
--- src/main/java/com/finndog/MVS/utils/StructureUtils.java
+++ src/main/java/com/finndog/MVS/utils/StructureUtils.java
@@ -1,39 +1,36 @@
 package com.finndog.mvs.utils;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.data.worldgen.StructureSets;
 import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.NoiseColumn;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.level.levelgen.structure.Structure;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class StructureUtils {
 
-//    public static boolean isOnWater(Structure.GenerationContext context) {
-//        ChunkPos chunkpos = context.chunkPos();
-//
-//        BlockPos centerOfChunk = chunkpos.getMiddleBlockPosition(0);
-//        int landHeight = context.chunkGenerator().getFirstOccupiedHeight(centerOfChunk.getX(), centerOfChunk.getZ(), Heightmap.Types.WORLD_SURFACE_WG, context.heightAccessor());
-//        NoiseColumn columnOfBlocks = context.chunkGenerator().getBaseColumn(centerOfChunk.getX(), centerOfChunk.getZ(), context.heightAccessor());
-//        BlockState topBlock = columnOfBlocks.getBlock(centerOfChunk.getY() + landHeight);
-//
-//        if(!topBlock.getFluidState().isEmpty()) {
-//            return true;
-//        }
-//        return false;
-//    }
-//
-//    public static boolean isAllowedTerrainHeightChange(Structure.GenerationContext context, Pair<Integer, Integer> size) {
-//        ChunkPos pos = context.chunkPos();
-//        int allowedTerrainHeightRange = 2;
-//
-//
-//        int[] cornerHeights = context.getCornerHeights(pos.getMiddleBlockX(), size.getFirst(), pos.getMiddleBlockZ(), size.getSecond());
-//
-//        int minHeight = Math.min(Math.min(cornerHeights[0], cornerHeights[1]), Math.min(cornerHeights[2], cornerHeights[3]));
-//        int maxHeight = Math.max(Math.max(cornerHeights[0], cornerHeights[1]), Math.max(cornerHeights[2], cornerHeights[3]));
-//
-//        return Math.abs(maxHeight - minHeight) <= allowedTerrainHeightRange;
-//    }
+    public static boolean onLiquid(Structure.GenerationContext context, boolean spawnInLiquid) {
+        ChunkPos chunkPos = context.chunkPos();
+        BlockPos centerOfChunk = chunkPos.getMiddleBlockPosition(0);
+        int landHeight = context.chunkGenerator().getFirstOccupiedHeight(centerOfChunk.getX(), centerOfChunk.getZ(), Heightmap.Types.WORLD_SURFACE_WG, context.heightAccessor(), context.randomState());
+        NoiseColumn columnOfBlocks = context.chunkGenerator().getBaseColumn(centerOfChunk.getX(), centerOfChunk.getZ(), context.heightAccessor(), context.randomState());
+        BlockState topBlock = columnOfBlocks.getBlock(centerOfChunk.getY() + landHeight);
 
+        if (spawnInLiquid) {
+            if(!topBlock.getFluidState().isEmpty()) {
+                return true;
+            }
+        }
+        if(topBlock.getFluidState().isEmpty()) {
+            return true;
+        }
+        return false;
+    }
     public static boolean isFeatureChunk(Structure.GenerationContext context, int distance) {
         ChunkPos chunkpos = context.chunkPos();
 
@@ -53,33 +50,20 @@ public class StructureUtils {
         return false;
     }
 
-//    public static int getSuitableNetherYLevel(Structure.GenerationContext context, BlockPos pos) {
-//        NoiseColumn column = context.chunkGenerator().getBaseColumn(pos.getX(), pos.getZ(), context.heightAccessor());
-//        List<Integer> suitableYLevels = new ArrayList<>();
-//
-//        for (int y = 127; y >= 32; y--) {
-//            if (column.getBlock(y - 1).canOcclude() && column.getBlock(y).isAir() && column.getBlock(y + 4).isAir() && column.getBlock(y + 8).isAir()) {
-//                suitableYLevels.add(y);
-//            }
-//        }
-//
-//        if (suitableYLevels.isEmpty()) {return 0;}
-//
-//        int yLevel = suitableYLevels.get(new Random(context.seed()).nextInt(suitableYLevels.size()));
-//        return yLevel;
-//    }
+    public static int getSuitableNetherYLevel(Structure.GenerationContext context) {
+        ChunkPos chunkPos = context.chunkPos();
+        NoiseColumn column = context.chunkGenerator().getBaseColumn(chunkPos.x, chunkPos.z, context.heightAccessor(), context.randomState());
+        List<Integer> suitableYLevels = new ArrayList<>();
 
-    public static boolean extraSpawningChecks(Structure.GenerationContext context) {
-        // Grabs the chunk position we are at
-        ChunkPos chunkpos = context.chunkPos();
+        for (int y = 127; y >= 32; y--) {
+            if (column.getBlock(y - 1).canOcclude() && column.getBlock(y).isAir() && column.getBlock(y + 4).isAir() && column.getBlock(y + 8).isAir()) {
+                suitableYLevels.add(y);
+            }
+        }
 
-        // Checks to make sure our structure does not spawn above land that's higher than y = 150
-        // to demonstrate how this method is good for checking extra conditions for spawning
-        return context.chunkGenerator().getFirstOccupiedHeight(
-                chunkpos.getMinBlockX(),
-                chunkpos.getMinBlockZ(),
-                Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                context.heightAccessor(),
-                context.randomState()) < 150;
+        if (suitableYLevels.isEmpty()) {return 0;}
+
+        int yLevel = suitableYLevels.get(new Random(context.seed()).nextInt(suitableYLevels.size()));
+        return yLevel;
     }
 }
