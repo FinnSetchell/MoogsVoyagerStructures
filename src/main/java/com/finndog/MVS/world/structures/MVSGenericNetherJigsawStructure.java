952e1330736dabe1f458ae3736b3adb475795ffa
FinnSetchell <88200552+FinnSetchell@users.noreply.github.com>
2022-10-06 22:24:09 +0100

    deleted unnecesary jigsaws, added spawnOnWater config

--- src/main/java/com/finndog/MVS/world/structures/MVSGenericNetherJigsawStructure.java
+++ src/main/java/com/finndog/MVS/world/structures/MVSGenericNetherJigsawStructure.java
@@ -9,7 +9,6 @@ import net.minecraft.core.Holder;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.levelgen.Heightmap;
-import net.minecraft.world.level.levelgen.WorldGenerationContext;
 import net.minecraft.world.level.levelgen.heightproviders.HeightProvider;
 import net.minecraft.world.level.levelgen.structure.Structure;
 import net.minecraft.world.level.levelgen.structure.StructureType;
@@ -20,8 +19,6 @@ import java.util.Optional;
 
 public class MVSGenericNetherJigsawStructure extends Structure {
 
-    // A custom codec that changes the size limit for our code_structure_sky_fan.json's config to not be capped at 7.
-    // With this, we can have a structure with a size limit up to 30 if we want to have extremely long branches of pieces in the structure.
     public static final Codec<MVSGenericNetherJigsawStructure> CODEC = RecordCodecBuilder.<MVSGenericNetherJigsawStructure>mapCodec(instance ->
             instance.group(MVSGenericNetherJigsawStructure.settingsCodec(instance),
                     StructureTemplatePool.CODEC.fieldOf("start_pool").forGetter(structure -> structure.startPool),
@@ -29,7 +26,8 @@ public class MVSGenericNetherJigsawStructure extends Structure {
                     Codec.intRange(0, 30).fieldOf("size").forGetter(structure -> structure.size),
                     HeightProvider.CODEC.fieldOf("start_height").forGetter(structure -> structure.startHeight),
                     Heightmap.Types.CODEC.optionalFieldOf("project_start_to_heightmap").forGetter(structure -> structure.projectStartToHeightmap),
-                    Codec.intRange(1, 128).fieldOf("max_distance_from_center").forGetter(structure -> structure.maxDistanceFromCenter)
+                    Codec.intRange(1, 128).fieldOf("max_distance_from_center").forGetter(structure -> structure.maxDistanceFromCenter),
+                    Codec.BOOL.fieldOf("spawn_in_liquid").orElse(false).forGetter(structure -> structure.spawnInLiquid)
             ).apply(instance, MVSGenericNetherJigsawStructure::new)).codec();
 
     private final Holder<StructureTemplatePool> startPool;
@@ -38,14 +36,17 @@ public class MVSGenericNetherJigsawStructure extends Structure {
     private final HeightProvider startHeight;
     private final Optional<Heightmap.Types> projectStartToHeightmap;
     private final int maxDistanceFromCenter;
+    public final boolean spawnInLiquid;
+
 
     public MVSGenericNetherJigsawStructure(Structure.StructureSettings config,
-                                           Holder<StructureTemplatePool> startPool,
-                                           Optional<ResourceLocation> startJigsawName,
-                                           int size,
-                                           HeightProvider startHeight,
-                                           Optional<Heightmap.Types> projectStartToHeightmap,
-                                           int maxDistanceFromCenter)
+                                     Holder<StructureTemplatePool> startPool,
+                                     Optional<ResourceLocation> startJigsawName,
+                                     int size,
+                                     HeightProvider startHeight,
+                                     Optional<Heightmap.Types> projectStartToHeightmap,
+                                     int maxDistanceFromCenter,
+                                     boolean spawnInLiquid)
     {
         super(config);
         this.startPool = startPool;
@@ -54,53 +55,20 @@ public class MVSGenericNetherJigsawStructure extends Structure {
         this.startHeight = startHeight;
         this.projectStartToHeightmap = projectStartToHeightmap;
         this.maxDistanceFromCenter = maxDistanceFromCenter;
+        this.spawnInLiquid = spawnInLiquid;
     }
-
-    /*
-     * This is where extra checks can be done to determine if the structure can spawn here.
-     * This only needs to be overridden if you're adding additional spawn conditions.
-     *
-     * Fun fact, if you set your structure separation/spacing to be 0/1, you can use
-     * extraSpawningChecks to return true only if certain chunk coordinates are passed in
-     * which allows you to spawn structures only at certain coordinates in the world.
-     *
-     * Basically, this method is used for determining if the land is at a suitable height,
-     * if certain other structures are too close or not, or some other restrictive condition.
-     *
-     * For example, Pillager Outposts added a check to make sure it cannot spawn within 10 chunk of a Village.
-     * (Bedrock Edition seems to not have the same check)
-     *
-     * If you are doing Nether structures, you'll probably want to spawn your structure on top of ledges.
-     * Best way to do that is to use getBaseColumn to grab a column of blocks at the structure's x/z position.
-     * Then loop through it and look for land with air above it and set blockpos's Y value to it.
-     * Make sure to set the final boolean in JigsawPlacement.addPieces to false so
-     * that the structure spawns at blockpos's y value instead of placing the structure on the Bedrock roof!
-     *
-     * Also, please for the love of god, do not do dimension checking here.
-     * If you do and another mod's dimension is trying to spawn your structure,
-     * the locate command will make minecraft hang forever and break the game.
-     * Use the biome tags for where to spawn the structure and users can datapack
-     * it to spawn in specific biomes that aren't in the dimension they don't like if they wish.
-     */
 
 
     @Override
     public Optional<Structure.GenerationStub> findGenerationPoint(Structure.GenerationContext context) {
-
-        // Check if the spot is valid for our structure. This is just as another method for cleanness.
-        // Returning an empty optional tells the game to skip this spot as it will not generate the structure.
-        if (!StructureUtils.extraSpawningChecks(context)) {
-            return Optional.empty();
-        }
-
-        // Set's our spawning blockpos's y offset to be 60 blocks up.
-        // Since we are going to have heightmap/terrain height spawning set to true further down, this will make it so we spawn 60 blocks above terrain.
-        // If we wanted to spawn on ocean floor, we would set heightmap/terrain height spawning to false and the grab the y value of the terrain with OCEAN_FLOOR_WG heightmap.
-        int startY = this.startHeight.sample(context.random(), new WorldGenerationContext(context.chunkGenerator(), context.heightAccessor()));
-
         // Turns the chunk coordinates into actual coordinates we can use. (Gets corner of that chunk)
         ChunkPos chunkPos = context.chunkPos();
+        int startY = StructureUtils.getSuitableNetherYLevel(context);
         BlockPos blockPos = new BlockPos(chunkPos.getMinBlockX(), startY, chunkPos.getMinBlockZ());
+
+        if (!StructureUtils.onLiquid(context, this.spawnInLiquid)) {return Optional.empty();}
+        if (StructureUtils.isFeatureChunk(context, 10)) {return Optional.empty();}
+
 
         Optional<Structure.GenerationStub> structurePiecesGenerator =
                 JigsawPlacement.addPieces(
@@ -115,12 +83,6 @@ public class MVSGenericNetherJigsawStructure extends Structure {
                         // Set this to false for structure to be place only at the passed in blockpos's Y value instead.
                         // Definitely keep this false when placing structures in the nether as otherwise, heightmap placing will put the structure on the Bedrock roof.
                         this.maxDistanceFromCenter); // Maximum limit for how far pieces can spawn from center. You cannot set this bigger than 128 or else pieces gets cutoff.
-
-        /*
-         * Note, you are always free to make your own JigsawPlacement class and implementation of how the structure
-         * should generate. It is tricky but extremely powerful if you are doing something that vanilla's jigsaw system cannot do.
-         * Such as for example, forcing 3 pieces to always spawn every time, limiting how often a piece spawns, or remove the intersection limitation of pieces.
-         */
 
         // Return the pieces generator that is now set up so that the game runs it when it needs to create the layout of structure pieces.
         return structurePiecesGenerator;
--- src/main/java/com/finndog/MVS/world/structures/MVSGenericSmallJigsawStructure.java
+++ src/main/java/com/finndog/MVS/world/structures/MVSGenericSmallJigsawStructure.java
@@ -20,8 +20,6 @@ import java.util.Optional;
 
 public class MVSGenericSmallJigsawStructure extends Structure {
 
-    // A custom codec that changes the size limit for our code_structure_sky_fan.json's config to not be capped at 7.
-    // With this, we can have a structure with a size limit up to 30 if we want to have extremely long branches of pieces in the structure.
     public static final Codec<MVSGenericSmallJigsawStructure> CODEC = RecordCodecBuilder.<MVSGenericSmallJigsawStructure>mapCodec(instance ->
             instance.group(MVSGenericSmallJigsawStructure.settingsCodec(instance),
                     StructureTemplatePool.CODEC.fieldOf("start_pool").forGetter(structure -> structure.startPool),
@@ -29,7 +27,8 @@ public class MVSGenericSmallJigsawStructure extends Structure {
                     Codec.intRange(0, 30).fieldOf("size").forGetter(structure -> structure.size),
                     HeightProvider.CODEC.fieldOf("start_height").forGetter(structure -> structure.startHeight),
                     Heightmap.Types.CODEC.optionalFieldOf("project_start_to_heightmap").forGetter(structure -> structure.projectStartToHeightmap),
-                    Codec.intRange(1, 128).fieldOf("max_distance_from_center").forGetter(structure -> structure.maxDistanceFromCenter)
+                    Codec.intRange(1, 128).fieldOf("max_distance_from_center").forGetter(structure -> structure.maxDistanceFromCenter),
+                    Codec.BOOL.fieldOf("spawn_in_liquid").orElse(false).forGetter(structure -> structure.spawnInLiquid)
             ).apply(instance, MVSGenericSmallJigsawStructure::new)).codec();
 
     private final Holder<StructureTemplatePool> startPool;
@@ -38,14 +37,17 @@ public class MVSGenericSmallJigsawStructure extends Structure {
     private final HeightProvider startHeight;
     private final Optional<Heightmap.Types> projectStartToHeightmap;
     private final int maxDistanceFromCenter;
+    public final boolean spawnInLiquid;
+
 
     public MVSGenericSmallJigsawStructure(Structure.StructureSettings config,
-                                          Holder<StructureTemplatePool> startPool,
-                                          Optional<ResourceLocation> startJigsawName,
-                                          int size,
-                                          HeightProvider startHeight,
-                                          Optional<Heightmap.Types> projectStartToHeightmap,
-                                          int maxDistanceFromCenter)
+                                     Holder<StructureTemplatePool> startPool,
+                                     Optional<ResourceLocation> startJigsawName,
+                                     int size,
+                                     HeightProvider startHeight,
+                                     Optional<Heightmap.Types> projectStartToHeightmap,
+                                     int maxDistanceFromCenter,
+                                     boolean spawnInLiquid)
     {
         super(config);
         this.startPool = startPool;
@@ -54,48 +56,15 @@ public class MVSGenericSmallJigsawStructure extends Structure {
         this.startHeight = startHeight;
         this.projectStartToHeightmap = projectStartToHeightmap;
         this.maxDistanceFromCenter = maxDistanceFromCenter;
+        this.spawnInLiquid = spawnInLiquid;
     }
-
-    /*
-     * This is where extra checks can be done to determine if the structure can spawn here.
-     * This only needs to be overridden if you're adding additional spawn conditions.
-     *
-     * Fun fact, if you set your structure separation/spacing to be 0/1, you can use
-     * extraSpawningChecks to return true only if certain chunk coordinates are passed in
-     * which allows you to spawn structures only at certain coordinates in the world.
-     *
-     * Basically, this method is used for determining if the land is at a suitable height,
-     * if certain other structures are too close or not, or some other restrictive condition.
-     *
-     * For example, Pillager Outposts added a check to make sure it cannot spawn within 10 chunk of a Village.
-     * (Bedrock Edition seems to not have the same check)
-     *
-     * If you are doing Nether structures, you'll probably want to spawn your structure on top of ledges.
-     * Best way to do that is to use getBaseColumn to grab a column of blocks at the structure's x/z position.
-     * Then loop through it and look for land with air above it and set blockpos's Y value to it.
-     * Make sure to set the final boolean in JigsawPlacement.addPieces to false so
-     * that the structure spawns at blockpos's y value instead of placing the structure on the Bedrock roof!
-     *
-     * Also, please for the love of god, do not do dimension checking here.
-     * If you do and another mod's dimension is trying to spawn your structure,
-     * the locate command will make minecraft hang forever and break the game.
-     * Use the biome tags for where to spawn the structure and users can datapack
-     * it to spawn in specific biomes that aren't in the dimension they don't like if they wish.
-     */
-
-
     @Override
     public Optional<Structure.GenerationStub> findGenerationPoint(Structure.GenerationContext context) {
 
-        // Check if the spot is valid for our structure. This is just as another method for cleanness.
-        // Returning an empty optional tells the game to skip this spot as it will not generate the structure.
-        if (!StructureUtils.extraSpawningChecks(context)) {
-            return Optional.empty();
-        }
+        if (!StructureUtils.onLiquid(context, this.spawnInLiquid)) {return Optional.empty();}
+        if (StructureUtils.isFeatureChunk(context, 10)) {return Optional.empty();}
 
-        // Set's our spawning blockpos's y offset to be 60 blocks up.
-        // Since we are going to have heightmap/terrain height spawning set to true further down, this will make it so we spawn 60 blocks above terrain.
-        // If we wanted to spawn on ocean floor, we would set heightmap/terrain height spawning to false and the grab the y value of the terrain with OCEAN_FLOOR_WG heightmap.
+
         int startY = this.startHeight.sample(context.random(), new WorldGenerationContext(context.chunkGenerator(), context.heightAccessor()));
 
         // Turns the chunk coordinates into actual coordinates we can use. (Gets corner of that chunk)
@@ -116,11 +85,6 @@ public class MVSGenericSmallJigsawStructure extends Structure {
                         // Definitely keep this false when placing structures in the nether as otherwise, heightmap placing will put the structure on the Bedrock roof.
                         this.maxDistanceFromCenter); // Maximum limit for how far pieces can spawn from center. You cannot set this bigger than 128 or else pieces gets cutoff.
 
-        /*
-         * Note, you are always free to make your own JigsawPlacement class and implementation of how the structure
-         * should generate. It is tricky but extremely powerful if you are doing something that vanilla's jigsaw system cannot do.
-         * Such as for example, forcing 3 pieces to always spawn every time, limiting how often a piece spawns, or remove the intersection limitation of pieces.
-         */
 
         // Return the pieces generator that is now set up so that the game runs it when it needs to create the layout of structure pieces.
         return structurePiecesGenerator;
