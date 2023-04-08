ffb2c882e57a81d1c9ec6ae7dee746207e3ba938
FinnSetchell <88200552+FinnSetchell@users.noreply.github.com>
2023-04-08 18:02:05 +0100

    backdated to 1.19.2

--- src/main/java/com/finndog/mvs/MVSStructures.java
+++ src/main/java/com/finndog/mvs/MVSStructures.java
@@ -3,9 +3,8 @@ package com.finndog.mvs;
 import com.finndog.mvs.world.structures.MVSGenericJigsawStructure;
 import com.finndog.mvs.world.structures.MVSGenericNetherJigsawStructure;
 import com.finndog.mvs.world.structures.Jigsaw;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import net.minecraft.world.gen.structure.StructureType;
 
 public class MVSStructures {
@@ -23,9 +22,8 @@ public class MVSStructures {
      * use them too directly from the registries. It's great for mod/datapacks compatibility.
      */
     public static void registerStructureFeatures() {
-        MVS_GENERIC_JIGSAW_STRUCTURE = Registry.register(Registries.STRUCTURE_TYPE, new Identifier(MVSMain.MODID, "mvs_generic_jigsaw_structure"), () -> MVSGenericJigsawStructure.CODEC);
-        MVS_GENERIC_NETHER_JIGSAW_STRUCTURE = Registry.register(Registries.STRUCTURE_TYPE, new Identifier(MVSMain.MODID, "mvs_generic_nether_jigsaw_structure"), () -> MVSGenericNetherJigsawStructure.CODEC);
-        JIGSAW = Registry.register(Registries.STRUCTURE_TYPE, new Identifier(MVSMain.MODID, "jigsaw"), () -> Jigsaw.CODEC);
-
+        MVS_GENERIC_JIGSAW_STRUCTURE = Registry.register(Registry.STRUCTURE_TYPE, new Identifier(MVSMain.MODID, "mvs_generic_jigsaw_structure"), () -> MVSGenericJigsawStructure.CODEC);
+        MVS_GENERIC_NETHER_JIGSAW_STRUCTURE = Registry.register(Registry.STRUCTURE_TYPE, new Identifier(MVSMain.MODID, "mvs_generic_nether_jigsaw_structure"), () -> MVSGenericNetherJigsawStructure.CODEC);
+        JIGSAW = Registry.register(Registry.STRUCTURE_TYPE, new Identifier(MVSMain.MODID, "jigsaw"), () -> Jigsaw.CODEC);
     }
 }
