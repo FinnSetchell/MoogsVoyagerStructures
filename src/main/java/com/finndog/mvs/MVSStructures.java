15b67a336a7f09a0a317ab5330483957100f997a
FinnSetchell <88200552+FinnSetchell@users.noreply.github.com>
2023-04-28 21:20:51 +0100

    registered a bunch of new stuff and changed deferred registry structure name

--- src/main/java/com/finndog/mvs/MVSStructures.java
+++ src/main/java/com/finndog/mvs/MVSStructures.java
@@ -7,7 +7,6 @@ package com.finndog.mvs;
 import com.finndog.mvs.world.structures.*;
 import com.mojang.serialization.Codec;
 import net.minecraft.core.Registry;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.world.level.levelgen.structure.Structure;
 import net.minecraft.world.level.levelgen.structure.StructureType;
 import net.minecraftforge.registries.DeferredRegister;
@@ -18,16 +17,16 @@ public class MVSStructures {
      * We are using the Deferred Registry system to register our structure as this is the preferred way on Forge.
      * This will handle registering the base structure for us at the correct time so we don't have to handle it ourselves.
      */
-    public static final DeferredRegister<StructureType<?>> DEFERRED_REGISTRY_STRUCTURE = DeferredRegister.create(Registries.STRUCTURE_TYPE, MVSMain.MODID);
+    public static final DeferredRegister<StructureType<?>> STRUCTURE_TYPE = DeferredRegister.create(Registry.STRUCTURE_TYPE_REGISTRY, MVSMain.MODID);
 
 
     /**
      * Registers the base structure itself and sets what its path is. In this case,
      * this base structure will have the resourcelocation of structure_tutorial:sky_structures.
      */
-    public static final RegistryObject<StructureType<MVSGenericJigsawStructure>> MVS_GENERIC_JIGSAW_STRUCTURE = DEFERRED_REGISTRY_STRUCTURE.register("mvs_generic_jigsaw_structure", () -> explicitStructureTypeTyping(MVSGenericJigsawStructure.CODEC));
-    public static final RegistryObject<StructureType<MVSGenericNetherJigsawStructure>> MVS_GENERIC_NETHER_JIGSAW_STRUCTURE = DEFERRED_REGISTRY_STRUCTURE.register("mvs_generic_nether_jigsaw_structure", () -> explicitStructureTypeTyping(MVSGenericNetherJigsawStructure.CODEC));
-    public static final RegistryObject<StructureType<Jigsaw>> JIGSAW = DEFERRED_REGISTRY_STRUCTURE.register("jigsaw", () -> explicitStructureTypeTyping(Jigsaw.CODEC));
+    public static final RegistryObject<StructureType<GenericJigsawStructure>> MVS_GENERIC_JIGSAW_STRUCTURE = STRUCTURE_TYPE.register("mvs_generic_jigsaw_structure", () -> explicitStructureTypeTyping(GenericJigsawStructure.CODEC));
+    public static final RegistryObject<StructureType<GenericNetherJigsawStructure>> MVS_GENERIC_NETHER_JIGSAW_STRUCTURE = STRUCTURE_TYPE.register("mvs_generic_nether_jigsaw_structure", () -> explicitStructureTypeTyping(GenericNetherJigsawStructure.CODEC));
+    public static final RegistryObject<StructureType<Jigsaw>> JIGSAW = STRUCTURE_TYPE.register("jigsaw", () -> explicitStructureTypeTyping(Jigsaw.CODEC));
 
     /**
      * Originally, I had a double lambda ()->()-> for the RegistryObject line above, but it turns out that
