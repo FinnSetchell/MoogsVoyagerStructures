380e0ce243ac00de2120e99aa28bd03cd5ea373c
FinnSetchell <88200552+FinnSetchell@users.noreply.github.com>
2023-07-05 00:20:53 +0100

    updated all of the fabric section

---
+++ fabric/src/main/java/com/finndog/mvs/fabric/RepurposedStructuresFabric.java
@@ -0,0 +1,29 @@
+package com.finndog.mvs.fabric;
+
+import com.finndog.mvs.MVSCommon;
+import com.finndog.mvs.events.lifecycle.RegisterReloadListenerEvent;
+import com.finndog.mvs.events.lifecycle.ServerGoingToStartEvent;
+import com.finndog.mvs.events.lifecycle.ServerGoingToStopEvent;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.minecraft.server.packs.PackType;
+
+public class RepurposedStructuresFabric implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        MVSCommon.init();
+
+        ServerLifecycleEvents.SERVER_STARTING.register(server -> {
+            ServerGoingToStartEvent.EVENT.invoke(new ServerGoingToStartEvent(server));
+
+        });
+
+        ServerLifecycleEvents.SERVER_STOPPING.register(server ->
+                ServerGoingToStopEvent.EVENT.invoke(ServerGoingToStopEvent.INSTANCE));
+
+        RegisterReloadListenerEvent.EVENT.invoke(new RegisterReloadListenerEvent((id, listener) ->
+                ResourceManagerHelper.get(PackType.SERVER_DATA).registerReloadListener(new FabricReloadListener(id, listener))));
+    }
+}
